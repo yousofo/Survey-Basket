f1ed4bc3e52e34286a9f1e0a819c5c19e7009bcb
yousefn <jon772002@gmail.com>
2024-05-14 06:55:46 +0300

    Add project files.

---
+++ Controllers/DevelopmentController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SurveyBasket.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DevelopmentController : ControllerBase
+    {
+        [HttpGet]
+        
+        public IActionResult Run()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Middlewares/CustomMiddleware.cs
@@ -0,0 +1,26 @@
+﻿namespace SurveyBasket.Middlewares
+{
+    public class CustomMiddleware
+    {
+        private readonly ILogger<CustomMiddleware> _logger;
+        private readonly RequestDelegate _next;
+
+        public CustomMiddleware(ILogger<CustomMiddleware> logger, RequestDelegate next)
+        {
+            _logger = logger;
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            _logger.LogInformation("custom middleware loging start");
+            _logger.LogInformation(context.Request.Method);
+            _logger.LogInformation(context.Request.Protocol);
+            _logger.LogInformation(context.Request.Host.Host);
+            _logger.LogInformation(context.Request.Host.Port.GetType().ToString());
+
+            await _next(context);
+
+            _logger.LogInformation("custom middleware loging end");
+        }
+    }
+}
